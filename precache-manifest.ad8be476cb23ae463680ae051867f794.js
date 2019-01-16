1c369a49149a5d5dee5eab14102e4b96c93036c2
Leandro Ungari <leandroungari@gmail.com>
2019-01-16 11:29:23 -0200

    Updates

---
+++ precache-manifest.ad8be476cb23ae463680ae051867f794.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c773335688ebc255c750",
+    "url": "/systematic-review-assistant/static/js/runtime~main.c7733356.js"
+  },
+  {
+    "revision": "2a53bf2f3e4f090472c5",
+    "url": "/systematic-review-assistant/static/js/main.2a53bf2f.chunk.js"
+  },
+  {
+    "revision": "0acdcf2d747d9fb89f71",
+    "url": "/systematic-review-assistant/static/js/1.0acdcf2d.chunk.js"
+  },
+  {
+    "revision": "5892e22d51ad3984aef69de8de3bdc1a",
+    "url": "/systematic-review-assistant/index.html"
+  }
+];
